9ce54917ceb7192ebd4df31484163cb26ebb2777
Raghu Tiwari <raghutiwari12345@gmail.com>
2018-05-25 20:58:24 +0530

    v1.2 with Strict Mode and Audio effects

--- js/main.js
+++ js/main.js
@@ -12,18 +12,41 @@ var checkTime=true;
 var seqwrong=0;
 var retryseq=[];
 var currentseq=0;
+var strict;
 $(document).ready(function(){
+	strict= $('.switch__toggle').is(":checked");
+	console.log(strict);
 	$('#start').click(function(){
-		if(flag==1){
-		flag=0;	
-		start();
+		strict= $('.switch__toggle').is(":checked");
+		$('.switch__toggle').attr('disabled',true);
+		if(strict){
+			$(this).addClass('startClass');
+			if(flag==1){
+			flag=0;	
+
+			startstrict();
+			}	
 		}
+		else {
+
+			$(this).addClass('startClass');
+			if(flag==1){
+			flag=0;	
+			start();
+			}	
+		}
+
+	});
+	$('.switch__toggle').on('checked',function(){
+		strict= $('.switch__toggle').is(":checked");
 
 	});
 function reset(){
 		flag=1;
 		count=0;
 		$('#countno').text(count);
+		$('#start').removeClass('startClass');
+
 		start();
 
 
@@ -32,11 +55,168 @@ function reset(){
 	$('#reset').click(function(){
 		flag=1;
 		count=0;
+		$('#start').removeClass('startClass');
 		$("#countno").text(count);
 		location.reload();
 		// start();
 	});
 
+function startstrict(){
+	checkTime=true;
+	right=0;
+	userseq='';
+	seq='';
+	userClicks=0;
+		count++;
+	$('#countno').text(count);
+	cur=count;
+	flagcur=cur;
+	if(count==20){
+		alert("Congratulations, You won");
+		return;
+	}else{
+			getSeqStrict();
+	}
+	// while(cur!==0){
+		
+		
+	// 	setTimeout(glowColor,3000,whichone);
+
+	// 	cur=cur-1;
+	// }
+}
+
+function getSeqStrict(){
+	$('#1,#2,#3,#4').attr('disabled','disabled');
+	// $('#1,#2,#3,#4').unbind('click');
+	setTimeout(function(){
+			var whichone=Math.floor(Math.random()*(5-1)+1).toString();
+			seq+=whichone;
+			switch(whichone){
+			case '1':
+			$('#simon1').get(0).play();
+			$('#'+whichone).addClass('activeGreen');
+			// $('#'+whichone).removeClass('activeGreen');
+			break;
+
+			case '2':
+			$('#simon2').get(0).play();
+			$('#'+whichone).addClass('activeBlue');	
+			// $('#'+whichone).removeClass('activeBlue');
+			break;
+
+			case '3':
+			$('#simon3').get(0).play();
+			$('#'+whichone).addClass('activeRed');
+			// $('#'+whichone).removeClass('activeRed');
+			break;
+
+			case '4':
+			$('#simon4').get(0).play();
+			$('#'+whichone).addClass('activeYellow');
+			// $('#'+whichone).removeClass('activeYellow');
+			break;
+
+			}
+
+			setTimeout(function(){
+			switch(whichone){
+			case '1':
+			$('#'+whichone).removeClass('activeGreen');
+			// $('#'+whichone).removeClass('activeGreen');
+			break;
+
+			case '2':
+			$('#'+whichone).removeClass('activeBlue');	
+			// $('#'+whichone).removeClass('activeBlue');
+			break;
+
+			case '3':
+			$('#'+whichone).removeClass('activeRed');
+			// $('#'+whichone).removeClass('activeRed');
+			break;
+
+			case '4':
+			$('#'+whichone).removeClass('activeYellow');
+			// $('#'+whichone).removeClass('activeYellow');
+			break;
+
+			}
+
+
+
+
+			},500);
+
+
+
+			console.log('Cur: '+ cur);
+		cur--;
+		if(cur>0){
+			getSeqStrict();
+		}
+		else{
+			currseq=seq;
+			console.log('currseq: '+ currseq);
+			getUserseqstrict(currseq);
+			//
+			// getUserseq();
+		}
+	},1200);
+
+
+
+
+};
+function getUserseqstrict(currseq){
+			$('#1,#2,#3,#4').removeAttr('disabled');
+				checkTime=false;
+				$('#1,#2,#3,#4').unbind('click').click(function(){
+				$('#simon'+this.id).get(0).play();
+					userseq+=this.id;
+					console.log('userseq:'+userseq);
+					flagcur--;
+					if(flagcur==0 && userseq==seq){
+						seqwrong=0;
+						console.log('userseq when correct: '+userseq);
+						console.log('Right');
+						right=1;
+						checkTime=true;
+						startstrict();
+						return;
+					}
+					else if(flagcur==0 && userseq!=seq){
+						console.log('Wrong');
+						right=0;
+						userseq='';
+						count=0;
+						alert("Wrong!");
+						startstrict();
+						return;
+					}
+					else if(!(seq.includes(userseq))){
+						console.log('Wrong');
+						right=0;
+						count=0;
+						alert("Wrong!");
+						console.log(seqwrong);
+						startstrict();
+						return;
+					}
+					});
+
+
+
+
+
+
+
+
+};
+
+
+
+//Strict Mode Off-----------------------------------------------------------------------------
 
 
 function start(){
@@ -65,26 +245,32 @@ function start(){
 }
 
 function getSeq(){
+	$('#1,#2,#3,#4').attr('disabled','disabled');
+	// $('#1,#2,#3,#4').unbind('click');
 	setTimeout(function(){
 			var whichone=Math.floor(Math.random()*(5-1)+1).toString();
 			seq+=whichone;
 			switch(whichone){
 			case '1':
+			$('#simon1').get(0).play();
 			$('#'+whichone).addClass('activeGreen');
 			// $('#'+whichone).removeClass('activeGreen');
 			break;
 
 			case '2':
+			$('#simon2').get(0).play();
 			$('#'+whichone).addClass('activeBlue');	
 			// $('#'+whichone).removeClass('activeBlue');
 			break;
 
 			case '3':
+			$('#simon3').get(0).play();
 			$('#'+whichone).addClass('activeRed');
 			// $('#'+whichone).removeClass('activeRed');
 			break;
 
 			case '4':
+			$('#simon4').get(0).play();
 			$('#'+whichone).addClass('activeYellow');
 			// $('#'+whichone).removeClass('activeYellow');
 			break;
@@ -139,8 +325,10 @@ function getSeq(){
 };
 
 function getUserseq(currseq){
+			$('#1,#2,#3,#4').removeAttr('disabled');
 				checkTime=false;
 				$('#1,#2,#3,#4').unbind('click').click(function(){
+					$('#simon'+this.id).get(0).play();
 					userseq+=this.id;
 					console.log('userseq:'+userseq);
 					flagcur--;
@@ -183,6 +371,7 @@ function getUserseq(currseq){
 };
 
 function retry(){
+		$('#1,#2,#3,#4').attr('disabled','disabled');
 	console.log(seq);
 	retryseq = seq.split('');
 	console.log(retryseq);
@@ -193,21 +382,25 @@ function retry(){
 			currentseq=retryseq[i];
 			switch(currentseq){
 			case '1':
+			$('#simon1').get(0).play();
 			$('#1').addClass('activeGreen');
 			// $('#'+whichone).removeClass('activeGreen');
 			break;
 
 			case '2':
+			$('#simon2').get(0).play();
 			$('#2').addClass('activeBlue');	
 			// $('#'+whichone).removeClass('activeBlue');
 			break;
 
 			case '3':
+			$('#simon3').get(0).play();
 			$('#3').addClass('activeRed');
 			// $('#'+whichone).removeClass('activeRed');
 			break;
 
 			case '4':
+			$('#simon4').get(0).play();
 			$('#4').addClass('activeYellow');
 			// $('#'+whichone).removeClass('activeYellow');
 			break;
